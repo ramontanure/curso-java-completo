44d6ffc0320a5a25452f52cc66c5de7e96329ed9
Ramon Tanure Carvalho <ramon_tanure_carvalho@hotmail.com>
2023-02-05 12:20:29 -0300

    Versão boa da solução do projeto de excecao relacionado a reserva

--- exercicios-aulas/excecoes/projeto_excecao_1/src/application/Program.java
+++ exercicios-aulas/excecoes/projeto_excecao_1/src/application/Program.java
@@ -6,51 +6,49 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	//ParseExcpetion diz que o metodo main pode lançar a exceção desse tipo e que não temos que tratar essa exceção
-	public static void main(String[] args) throws ParseException {
-		
+	// ParseExcpetion diz que o metodo main pode lançar a exceção desse tipo e que
+	// não temos que tratar essa exceção
+	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		System.out.print("Room number: ");
-		int number = sc.nextInt();
-		
-		/*Quando temos um metodo que pode gerar uma exceção,
-		 * podemos tratar com try catch ou propagar uma exceção
-		*/
-		System.out.print("Check-in date (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(sc.next());	
-		
-		System.out.print("Check-out date (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(sc.next());
-		
-		if(!checkOut.after(checkIn)) {
-			System.out.println("Error in reservaton: Check-out date must be after check-in date");
-		} else {
+
+		/*
+		 * Quando temos um metodo que pode gerar uma exceção, podemos tratar com try
+		 * catch ou propagar uma exceção
+		 */
+
+		try {
+			System.out.print("Room number: ");
+			int number = sc.nextInt();
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
+
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
-			
+
 			System.out.println("Reservation: " + reservation);
-		
+
 			System.out.println();
 			System.out.println("Enter data to update the reservation:");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
-			checkIn = sdf.parse(sc.next());	
-			
+			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
-			
-			String error = reservation.updateDates(checkIn, checkOut);
-			
-			if(error != null) {
-				System.out.println(error);
-			} else {
-				System.out.println("Reservation: " + reservation);
-			}
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+		} catch (ParseException e) {
+			System.out.println("Invalid date format.");
+		} catch (DomainException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
+		} catch (RuntimeException e) {
+			System.out.println("Unexpected error");
 		}
-		
 		sc.close();
 	}
 
